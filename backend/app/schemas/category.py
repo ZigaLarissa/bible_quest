21f2dac7ddebb42ef559b44888267679c0c5f344
ZigaLarissa <ziga0902larissa@gmail.com>
2024-07-22 11:04:00 +0200

    category schemas

---
+++ backend/app/schemas/category.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+# Category Schemas
+class CategoryBase(BaseModel):
+    name: str
+
+class CategoryCreate(CategoryBase):
+    pass
+
+class Category(CategoryBase):
+    id: int
+    
+    class Config:
+        orm_mode = True
