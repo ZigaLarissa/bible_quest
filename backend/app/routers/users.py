f49f5ee68222ed3860f16c1c7468ad0390bb6b01
ZigaLarissa <ziga0902larissa@gmail.com>
2024-07-22 12:19:54 +0200

    users fastapi endpoints / routes

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -1,7 +1,43 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from schemas.user import User, UserList, UserCreate
+from crud import get_users, get_user_by_email, get_user, create_user, delete_user
+
+from services.database import SessionLocal
+
 
 router = APIRouter()
 
-@router.get("/users/", tags=["users"])
-async def read_users():
-    return [{"username": "Rick"}, {"username": "Morty"}]
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.post("/users/", response_model=User , tags=["users"])
+async def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return create_user(db=db, user=user)
+
+
+@router.get("/users/", response_model=UserList , tags=["users"])
+async def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    users = get_users(db, skip=skip, limit=limit)
+    return {"users": users}
+
+@router.get("/user/{user_id}", response_model=User , tags=["users"])
+async def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+@router.delete("/user/{user_id}", response_model=User , tags=["users"])
+async def delete_user(user_id: int):
+    return {"user_id": user_id}
