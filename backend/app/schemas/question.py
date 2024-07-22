eb990d6bad8bc218be700920eb25d3f1a7bdeef0
ZigaLarissa <ziga0902larissa@gmail.com>
2024-07-22 11:04:11 +0200

    questions schemas

---
+++ backend/app/schemas/question.py
@@ -0,0 +1,21 @@
+from pyndantic import BaseModel
+from schemas.category import Category
+
+# Question Schemas
+class QuestionBase(BaseModel):
+    question_text: str
+    correct_answer: str
+    incorrect_answer1: str
+    incorrect_answer2: str
+    incorrect_answer3: str
+
+class QuestionCreate(QuestionBase):
+    category_id: int
+
+class Question(QuestionBase):
+    id: int
+    category_id: int
+    category: Category
+
+    class Config:
+        orm_mode = True
