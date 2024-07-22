134fc5c40b3744f4abc1c3343a22aefa45a232c0
ZigaLarissa <ziga0902larissa@gmail.com>
2024-07-22 11:04:29 +0200

    users scheamas

---
+++ backend/app/schemas/user.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, EmailStr
+
+# User Schemas
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+
+class UserCreate(UserBase):
+    password: str
+
+class UserInDB(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class User(UserInDB):
+    pass
