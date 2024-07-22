62d8561603db319d91ffaf5bfcb759203d9e9e92
ZigaLarissa <ziga0902larissa@gmail.com>
2024-07-22 12:19:20 +0200

    communicate with the users table on the db

---
+++ backend/app/crud.py
@@ -0,0 +1,30 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+# User CRUD operations
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).Offset(skip).limit(limit).all()
+
+def create_user(db: Session, user: schemas.UserCreate):
+    hashed_password = user.password + "notreallyhashed"
+    db_user = models.User(email=user.email, hashed_password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def delete_user(db: Session, user_id: int):
+    db_user = db.query(models.User).filter(models.User.id == user_id).first()
+    if db_user:
+        db.delete(db_user)
+        db.commit()
+    return db_user
+
+
+# Category CRUD operations
