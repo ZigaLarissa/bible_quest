f8cf212f22e3b1383576f61542954ec9c155a92a
ZigaLarissa <ziga0902larissa@gmail.com>
2024-08-27 11:05:25 +0200

    inside a script

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 
 from routers import users
 
+import uvicorn
+
 app = FastAPI()
 
 app.include_router(users.router)
@@ -9,3 +11,7 @@ app.include_router(users.router)
 @app.get("/")
 async def root():
     return {"message": "Bible Questions API"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
